ea1d698fc9156b469bffc2562168a6496fa6614a
bhupeshdewangan11 <dewabhupesh@gmail.com>
2022-09-24 12:40:05 +0530

    1

--- spring-rest-demo/src/main/java/com/bhupesh/springdemo/config/DemoAppConfig.java
+++ spring-rest-demo/src/main/java/com/bhupesh/springdemo/config/DemoAppConfig.java
@@ -1,5 +1,12 @@
 package com.bhupesh.springdemo.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.bhupesh.springdemo")
 public class DemoAppConfig {
 
 }
