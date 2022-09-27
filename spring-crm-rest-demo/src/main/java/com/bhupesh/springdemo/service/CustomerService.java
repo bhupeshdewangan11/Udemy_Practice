108cd31188982a82ddd540ab30b407ffef31c47e
bhupeshdewangan11 <dewabhupesh@gmail.com>
2022-09-27 11:00:20 +0530

    initial

---
+++ spring-crm-rest-demo/src/main/java/com/bhupesh/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.bhupesh.springdemo.service;
+
+import java.util.List;
+
+import com.bhupesh.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
