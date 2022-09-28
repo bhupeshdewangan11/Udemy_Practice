0f2941c9d69ace49273a35e6a115b95696957cc4
bhupeshdewangan11 <dewabhupesh@gmail.com>
2022-09-28 15:38:45 +0530

    1

---
+++ 01-spring-boot-demo/src/test/java/com/bhupesh/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhupesh.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
